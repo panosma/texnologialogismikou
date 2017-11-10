a7877c4b31ff49a394b4c8e2cae7bd256581b0f1
panos_ziros <panos.ziros@gmail.com>
2017-11-11 00:42:26 +0200

    Adding_new_app

---
+++ BestOffer/app/src/main/java/bestoffer/tl/com/bestoffer/Result_list.java
@@ -0,0 +1,13 @@
+package bestoffer.tl.com.bestoffer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Result_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_list);
+    }
+}
